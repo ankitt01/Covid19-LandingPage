48ee02b12bcdc2a59ac25ce3256001cd5e4481b3
ankitt01 <ankitrawat0102@gmail.com>
2020-12-13 20:17:32 +0530

    small changes

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const cards = document.querySelectorAll('#anim');
 
 var observer = new IntersectionObserver((entries) => {
-    console.log(entries)
-
     entries.forEach(entry => {
         if(entry.intersectionRatio > 0 && entry.target.dataset.direction === 'vertical'){
         entry.target.style.animation = `animvertical 1s ${entry.target.dataset.delay} forwards ease-out`;
@@ -24,9 +22,33 @@ cards.forEach(card => {
 const ham = document.querySelector('.ham') 
 const close = document.querySelector('.close')
 const navLinks = document.querySelector('.nav-links')
+const links = document.querySelectorAll('.link a')
 ham.addEventListener('click', () => {
     navLinks.style.transform = 'translateX(0)';
 })
 close.addEventListener('click', () => {
     navLinks.style.transform = 'translateX(100%)';
 })
+links.forEach((link) => {
+    link.addEventListener('click' , () => {
+        navLinks.style.transform = 'translateX(100%)';
+    })
+})
+
+
+const navbar = document.querySelector('nav')
+const hero = document.querySelector('.hero-container')
+const options1 = {
+    rootMargin: "-100px"
+};
+const observer1 = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+            navbar.classList.add('nav-scrolled')
+        } else {
+            navbar.classList.remove('nav-scrolled')
+        }
+    })
+}, options1)
+
+observer1.observe(hero);
