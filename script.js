f60c337e3c2980e9bd326b5ae93c38d8d929a765
ankitt01 <ankitrawat0102@gmail.com>
2020-12-13 11:02:11 +0530

    navigation added

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ var observer = new IntersectionObserver((entries) => {
     console.log(entries)
 
     entries.forEach(entry => {
-        if(entry.intersectionRatio > 0){
-        entry.target.style.animation = `anim1 1s ${entry.target.dataset.delay} forwards ease-out`;
+        if(entry.intersectionRatio > 0 && entry.target.dataset.direction === 'vertical'){
+        entry.target.style.animation = `animvertical 1s ${entry.target.dataset.delay} forwards ease-out`;
+    } else if(entry.intersectionRatio > 0 && entry.target.dataset.direction === 'horizontal'){
+        entry.target.style.animation = `animhorizontal 1s ${entry.target.dataset.delay} forwards ease-out`;
     }
     else {
         entry.target.style.animation = 'none';
@@ -16,4 +18,15 @@ var observer = new IntersectionObserver((entries) => {
 
 cards.forEach(card => {
     observer.observe(card);
+})
+
+
+const ham = document.querySelector('.ham') 
+const close = document.querySelector('.close')
+const navLinks = document.querySelector('.nav-links')
+ham.addEventListener('click', () => {
+    navLinks.style.transform = 'translateX(0)';
+})
+close.addEventListener('click', () => {
+    navLinks.style.transform = 'translateX(100%)';
 })
