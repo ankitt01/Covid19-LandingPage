6496b5dfee1b43a3ed3b127fef24cdc9dd122fb8
ankitt01 <ankitrawat0102@gmail.com>
2020-12-23 09:38:48 +0530

    added API and fetched its data

--- script.js
+++ script.js
@@ -52,3 +52,19 @@ const observer1 = new IntersectionObserver((entries) => {
 }, options1)
 
 observer1.observe(hero);
+
+
+const totalCases = document.getElementById('total-cases')
+const deaths = document.getElementById('deaths')
+const recovered = document.getElementById('recovered')
+
+fetch('https://api.covid19api.com/summary')        
+        .then((res) => {
+            return res.json()
+        })
+        .then((data) => {
+            console.log(data.Countries[76])
+            totalCases.textContent = `${data.Countries[76].TotalConfirmed}`
+            deaths.textContent =  `${data.Countries[76].TotalDeaths}`
+            recovered.textContent = `${data.Countries[76].TotalRecovered}`
+        })
